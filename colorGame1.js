3afc516775f89c88bb77656e57304a566605d565
tommywrightwolves <79713972+tommywrightwolves@users.noreply.github.com>
2022-12-13 10:50:13 +0000

    Complete color game challenge

---
+++ colorGame1.js
@@ -0,0 +1,126 @@
+var numberOfSquares = 6;
+var colors = generateRandomColor(numberOfSquares);
+var squares = document.querySelectorAll(".square");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
+
+for (var i = 0; i < modeButtons.length; i++){
+	modeButtons[i].addEventListener("click", function(){
+		modeButtons[0].classList.remove("selected");
+		modeButtons[1].classList.remove("selected");
+		this.classList.add("selected");
+		this.textContent === "Easy" ? numberOfSquares = 3: numberOfSquares = 6;
+		
+
+		reset();
+});
+}
+
+
+
+
+function reset(){
+
+
+colors = generateRandomColor(numberOfSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	this.textContent = "New Colors"
+	messageDisplay.textContent = "";
+
+	for(var i = 0; i < squares.length; i++) {
+		if(colors[i]){
+			squares[i].style.display = "block";
+			squares[i].style.backgroundColor = colors[i]
+		} else {
+			squares[i].style.display = "none";
+		}
+		
+
+
+	}
+	h1.style.backgroundColor = "steelblue";
+
+}
+
+
+
+
+	
+
+
+
+
+
+
+
+resetButton.addEventListener("click", function(){
+
+reset();
+})
+
+colorDisplay.textContent = pickedColor;
+ 
+ 
+for(var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = colors[i];
+
+	squares[i].addEventListener("click", function(){
+		var clickedColor = this.style.backgroundColor;
+		console.log(clickedColor, pickedColor);
+		if (clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			resetButton.textContent = "Play Again"
+			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
+		}
+		else{
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again"
+		}
+
+	});
+}
+
+function changeColors(color){
+	for(var i = 0; i < squares.length; i++){
+	squares[i].style.backgroundColor = color;
+}
+
+}
+
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColor(num){
+var arr = [];
+	for (var i = 0; i < num; i++){
+		arr.push(randomColor())
+
+
+	}
+
+
+return arr;
+
+}
+function randomColor(){
+
+	var r = Math.floor(Math.random() * 256);
+
+	var g = Math.floor(Math.random() * 256);
+
+	var b = Math.floor(Math.random() * 256)
+
+	return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+
+
+
